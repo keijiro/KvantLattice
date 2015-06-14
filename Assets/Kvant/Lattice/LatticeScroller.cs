49cff60c7ba2f98475f53be31b501e8e2a3c3491
Keijiro Takahashi <keijiro@ZX.local>
2015-06-14 20:59:38 +0900

    Replaced public variables with public properties.

--- Assets/Kvant/Lattice/LatticeScroller.cs
+++ Assets/Kvant/Lattice/LatticeScroller.cs
@@ -9,14 +9,27 @@ namespace Kvant
     [AddComponentMenu("Kvant/Lattice Scroller")]
     public class LatticeScroller : MonoBehaviour
     {
-        public float yawAngle;
-        public float speed;
+        [SerializeField]
+        float _yawAngle;
+
+        public float yawAngle {
+            get { return _yawAngle; }
+            set { _yawAngle = value; }
+        }
+
+        [SerializeField]
+        float _speed;
+
+        public float speed {
+            get { return _speed; }
+            set { _speed = value; }
+        }
 
         void Update()
         {
-            var r = yawAngle * Mathf.Deg2Rad;
+            var r = _yawAngle * Mathf.Deg2Rad;
             var dir = new Vector2(Mathf.Cos(r), Mathf.Sin(r));
-            GetComponent<Lattice>().offset += dir * speed * Time.deltaTime;
+            GetComponent<Lattice>().offset += dir * _speed * Time.deltaTime;
         }
     }
 }
